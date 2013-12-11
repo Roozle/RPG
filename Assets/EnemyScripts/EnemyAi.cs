be190abd1cd2b37d5ed2d2224f5d715de1adcf4d
Liam Paul Dakin MA <tutoeth@gmail.com>
2013-12-11 16:36:53 +0000

    Start

---
+++ Assets/EnemyScripts/EnemyAi.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAi : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+	private Transform myTransform;
+
+	void Awake ()
+	{
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+		target = go.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(target.position, myTransform.position, Color.yellow);
+	}
+}
